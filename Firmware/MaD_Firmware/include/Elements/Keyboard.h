5f6843fad8b69dfdfd6bdb3169ebdc1b15bee13b
RileyMcCarthy <59575827+RileyMcCarthy@users.noreply.github.com>
2021-12-09 16:45:09 -0500

    Update Keyboard.h
    fixed keyboard object to use dynamic memory instead of static, may be reverted

--- Firmware/MaD_Firmware/include/Elements/Keyboard.h
+++ Firmware/MaD_Firmware/include/Elements/Keyboard.h
@@ -1,17 +1,26 @@
 #ifndef Keyboard_H
 #define Keyboard_H
-#ifdef __MEMORY_CHECK__
-#include "leak_detector_c.h"
-#endif
+
 #include "simpletools.h"
 #include "RA8876.h"
 #include "Images.h"
+
+typedef struct keyboard_s
+{
+    Display *display;
+    Images *images;
+    Button *keys;
+    char *keyboard_buffer;
+    bool keyboard_complete;
+} Keyboard;
+
+Keyboard *keyboard_create(Display *display, Images *images);
+void keyboard_destroy(Keyboard *keyboard);
 
 /**
  * @brief The keyboard class contains an ondisplay keyboard that can be used to gather user input
  *
  **/
-
-char *keyboard_get_input(Display *display, const char *prompt);
+char *keyboard_get_input(Keyboard *keyboardObj, const char *prompt);
 
 #endif
